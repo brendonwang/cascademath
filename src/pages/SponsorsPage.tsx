d5437248eb5eb8656b1128ad44af68db2f65aeeb
lw-2233 <laura.j.wang@gmail.com>
2026-08-24 22:29:57 -0700

    Update SponsorsPage.tsx (#25)

--- src/pages/SponsorsPage.tsx
+++ src/pages/SponsorsPage.tsx
@@ -79,7 +79,7 @@ export function SponsorsPage() {
                             target="_blank"
                             rel="noreferrer"
                           >
-                            <image src={sponsor.logo} height="150" width="150" />
+                            <span className={sponsorNameClass}>{sponsor.name}</span>
                             <ArrowUpRight className="size-4 shrink-0 text-primary" aria-hidden="true" strokeWidth={2} />
                           </a>
                         ) : (
